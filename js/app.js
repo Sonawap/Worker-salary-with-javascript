dbcfa6ff8de9bf2c6c95b0c5a7f85c0b4f8f5ffb
Sonawap <paulsola79@gmail.com>
2020-10-31 16:43:13 -0700

    new

--- js/app.js
+++ js/app.js
@@ -5,8 +5,11 @@ new Vue({
         /////assigning default values to data
         hours:0,
         extrahours:0,
-        price:500,
+        price:1000,
+        extraPrice: 200,
         earn: 0,
+        xearn : 0,
+        tearn: 0,
         error: false
 
     },
@@ -16,18 +19,18 @@ new Vue({
 
             if(this.extrahours == 0){
                 this.earn = parseInt(this.hours) * parseInt(this.price)
+                this.tearn = parseInt(this.hours) * parseInt(this.price)
             }
 
             // Calculate the Employee Earning witho extra Hours
             if(this.extrahours > 0){
-                var totalHours = parseInt(this.hours) + parseInt(this.extrahours)
-                this.earn =  totalHours * this.price
+                var normalHours = parseInt(this.hours) * parseInt(this.price)
+                var extraHours = parseInt(this.extrahours) * parseInt(this.extraPrice)
+                this.xearn = extraHours
+                this.earn =  normalHours
+                this.tearn = normalHours + extraHours
             }
         },
-        validateInput(){
-            if(this.hours < 0){
-                console.log('tag', '')
-            }
-        }
+
     }
 });
